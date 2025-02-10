d9335c2f9d88af81b186e640a4a69e785eae434b
Ayanokoji4328 <guptaabhi0520@gmail.com>
2025-02-10 19:26:00 +0530

    4th practical

--- p4.py
+++ p4.py
@@ -3,12 +3,9 @@ def merge_sort(arr):
         mid = len(arr) // 2
         left_half = arr[:mid]
         right_half = arr[mid:]
-
         merge_sort(left_half)
         merge_sort(right_half)
-
         i = j = k = 0
-
         while i < len(left_half) and j < len(right_half):
             if left_half[i] < right_half[j]:
                 arr[k] = left_half[i]
@@ -17,24 +14,21 @@ def merge_sort(arr):
                 arr[k] = right_half[j]
                 j += 1
             k += 1
-
         while i < len(left_half):
             arr[k] = left_half[i]
             i += 1
             k += 1
-
         while j < len(right_half):
             arr[k] = right_half[j]
             j += 1
             k += 1
-
-def merge_two_sorted_lists(list1, list2):
+def merge_and_sort_lists(list1, list2):
     merged_list = list1 + list2
     merge_sort(merged_list)
     return merged_list
-
-list1 = [34, 7, 23, 32, 5]
-list2 = [78, 12, 3, 56, 89]
-
-sorted_list = merge_two_sorted_lists(list1, list2)
-print("Sorted Merged List:", sorted_list)
+list1 = list(map(int, input("Enter element of 1st list: ").split()))
+list2 = list(map(int, input("Enter element of 2nd list: ").split()))
+sorted_list = merge_and_sort_lists(list1, list2)
+print("List 1 is: ", list1)
+print("List 2 is: ", list2)
+print("Sorted merged list is: ", sorted_list)
